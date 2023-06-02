9cffb52200ac606148a32754db8d92bbcaaa62e0
yang.wang <yang.wang@xtransfer.cn>
2023-06-03 00:57:05 +0800

    chore: 删除无用代码

--- src/global.d.ts
+++ src/global.d.ts
@@ -5,12 +5,6 @@
 // declare module "*.gif";
 // declare module "*.svg";
 
-// 导入 SCSS 模块
-declare module '*.scss' {
-  const content: { [className: string]: string };
-  export default content;
-}
-
 // 导入样式模块（SCSS）
 declare module '*.module.scss' {
   const classes: { [className: string]: string };
